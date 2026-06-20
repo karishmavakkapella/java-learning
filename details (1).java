c2a3c83a2a0f42edaf3317b4beb91620cc079bad
karishmavakkapella <karishmavakkapella@gmail.com>
2026-06-20 18:26:02 +0530

    added detais.java file

---
+++ details (1).java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("name : V.Karishma");
+		System.out.println("course : b-tech");
+		System.out.println("branch : AIML");
+	}
+}
