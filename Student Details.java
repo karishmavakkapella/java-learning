75cb171139e5a72fb53209f7ff3202dce820cbe4
karishmavakkapella <karishmavakkapella@gmail.com>
2026-06-21 16:40:34 +0530

    Add files via upload
    added student detais file

---
+++ Student Details.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		String name =" Karishma";
+		int age = 18;
+  	double CGPA = 9.0;
+  	char grade = 'A';
+		System.out.println("-----student details---------");
+		System.out.println("name : "+name);
+		System.out.println("age : "+age);
+		System.out.println("CGPA : "+CGPA);
+		System.out.println( "grade : "+grade);
+	}
+}
