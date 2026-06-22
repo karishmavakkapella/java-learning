e1dc2f34c73933c4a68512e0f7b63f5f269008ec
karishmavakkapella <karishmavakkapella@gmail.com>
2026-06-22 21:56:56 +0530

    Add files via upload
    added celcious to fahrenheit converting file

---
+++ celcious.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int fahrenheit = sc.nextInt();
+	double celcious =5* (fahrenheit-32)/9;
+//	int Celcious= (int)celcious;
+	System.out.println("celcious : "+celcious);	
+	}
+}
