6acd05b8b7e3ce56803e42cb5b42a2567ba6253e
karishmavakkapella <karishmavakkapella@gmail.com>
2026-06-22 21:58:22 +0530

    added fahrenheit to celcious converting  file

---
+++ Fahrenheit.java
@@ -0,0 +1,11 @@
+//celcious to fahrenhiet
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int celcious = sc.nextInt();
+	double Fahrenheit = (9*celcious/5)+32;
+//	int farenheit = (int)F;
+	System.out.println("Farenheit : "+Fahrenheit);	
+	}
+}
