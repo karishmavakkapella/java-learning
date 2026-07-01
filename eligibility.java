bc3f6a3dfcfd068a77daf3f0145b25c5a04f1b34
Karishma Vakkapella <karishmavakkapella@gmail.com>
2026-07-01 12:55:59 +0530

    added voting eligibility file

---
+++ eligibility.java
@@ -0,0 +1,11 @@
+//voting eligibility
+public class Main {
+	public static void main(String[] args) {
+	int n = 20;
+	if(n>=18){
+		System.out.println("Eligible");
+		}else{
+			System.out.println("not Eligible");
+		}
+	}
+}
