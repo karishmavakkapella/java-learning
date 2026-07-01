84def093932d3fe7e306ef00d532889ed6a8cce9
Karishma Vakkapella <karishmavakkapella@gmail.com>
2026-07-01 12:45:52 +0530

    Add files via upload
    added finding sum file

---
+++ sum.java
@@ -0,0 +1,11 @@
+//sum of two numbers
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter two numbers to add : ");int a = sc.nextInt();
+	int b = sc.nextInt();
+	int sum = a+b;//add two numbers
+	System.out.println("Sum : "+sum);	
+	}
+}
