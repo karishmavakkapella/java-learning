f3155cdd8cc185b572028d874dd153212b69ee37
Karishma Vakkapella <karishmavakkapella@gmail.com>
2026-07-01 13:04:13 +0530

    added all arithmetic operations file

---
+++ arithmetic Operator.java
@@ -0,0 +1,12 @@
+//all arithimetic operations
+public class Main {
+	public static void main(String[] args) {
+	int a = 10;
+	int b = 5;
+	System.out.println(a+b);//sum
+	System.out.println(a-b);//subtract
+	System.out.println(a*b);//multiply
+	System.out.println(a/b);//division
+	System.out.println(a%b);//modulus	
+	}
+}
