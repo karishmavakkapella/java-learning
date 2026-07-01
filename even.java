c20caa34da4a86c32be5cb6e4c6f3bc01c27e445
Karishma Vakkapella <karishmavakkapella@gmail.com>
2026-07-01 12:52:03 +0530

    added finding even or odd file

---
+++ even.java
@@ -0,0 +1,14 @@
+//check even or odd
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter a number :");
+	int n = sc.nextInt();
+	if(n%2==0){
+		System.out.println("Even number : ");
+		}else{
+			System.out.println("odd number : ");
+			}	
+	}
+}
