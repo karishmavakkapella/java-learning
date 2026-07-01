1645f4fec66b08db2bef36026b54c5d111317bc2
Karishma Vakkapella <karishmavakkapella@gmail.com>
2026-07-01 12:48:24 +0530

    added finding largest number among two numbers file

---
+++ largestOfTwoNumbers.java
@@ -0,0 +1,15 @@
+//largest of two numbers
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter two numbers :");
+int a = sc.nextInt();
+int b = sc.nextInt();
+if (a>b){
+	System.out.println(a +" is the largest ");
+	}else{
+		System.out.println(b + " is the largest ");
+		}	
+	}
+}
