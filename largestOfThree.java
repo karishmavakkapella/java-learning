19f5675bcb9201a99a12a324b6e3ba892cc0caba
Karishma Vakkapella <karishmavakkapella@gmail.com>
2026-07-01 12:50:23 +0530

    added finding largest number among three numbers file

---
+++ largestOfThree.java
@@ -0,0 +1,21 @@
+//largest of three numbers
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter three numbers :");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		if(a==b&&a==c){
+			System.out.println("equal numbers :");
+		}
+	else if(a>b&&a>c){
+			System.out.println(a +" is the largest :");
+		}else if(b>a&&b>c){
+			System.out.println(b +" is the largest :");
+		}else{
+			System.out.println(c +" is the laegest :");
+		}
+	}
+}
