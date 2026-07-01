1c931a0921e8d573504bb0b5056ceee1ec3ec1c0
Karishma Vakkapella <karishmavakkapella@gmail.com>
2026-07-01 12:56:55 +0530

    Add files via upload
    added AND operator file

---
+++ ANDOperator.java
@@ -0,0 +1,14 @@
+//number div by 5 and 11
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number : ");
+	int n = sc.nextInt();
+	if(n%5==0&&n%11==0){
+		System.out.println(n + " is divisible by both 5 and 11 ");
+		}else{
+	System.out.println(n + " is not divisible by both 5 and 11 ");
+	}	
+	}
+}
